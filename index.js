839c270038f7c489043f664a868d43cb6869256b
Michael Elfassy <mike.elfassy@gmail.com>
2022-12-02 00:25:31 -0500

    commit tree ee2d4adcb9162d48015c6110adcc41c0d4e64a8b

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = function() {
+  return tutorials.map(sentence => {
+    let words = sentenceToWords(sentence)
+    let newSentence = ''
+    for(let word of words) {
+      newSentence += titleCaseWord(word) + ' '
+    }
+    return newSentence.trim()
+  })
+}
+
+const sentenceToWords = function(sentence) {
+  const words = ['']
+  let wordCount = 0
+  for(const letter of sentence) {
+    if (letter !== ' ') {
+      words[wordCount] += letter
+    }
+    else {
+      wordCount++
+      words.push('')
+    }
+  }
+  return words
+};
+
+const titleCaseWord = function(word) {
+  return word.slice(0,1).toUpperCase() + word.slice(1,word.length)
 }
